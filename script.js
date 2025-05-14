86e1f39b6b9e177e7fd83af61c7fc1b6307170d4
Iliya Belyak <iliyabel2003@outlook.com>
2025-05-14 03:26:35 -0700

    Updated dollar img size. added working assists + correct counting.

--- script.js
+++ script.js
@@ -1,22 +1,72 @@
-let money_click_value = 1;
+let money_click_value = 1.0;
+let assist_rate = 0;
+assist_started = false;
+
+let assists = {
+    pen: { price: 10, rate: 0.1, active: false, id: "pen-assist" },
+    printAndPress: { price: 50, rate: 1, active: false, id: "print-assist" },
+    printer: { price: 200, rate: 5, active: false, id: "printer-assist" },
+    moneyTree: { price: 1000, rate: 20, active: false, id: "tree-assist" },
+    farm: { price: 5000, rate: 100, active: false, id: "farm-assist" },
+};
+
+
 
 function moneyMint() {
-    let count = parseInt(document.getElementById("money-total").textContent);
+    let count = parseFloat(document.getElementById("money-total").textContent);
     count = count + money_click_value;
     document.getElementById("money-total").textContent = count;
 }
 
 function penUpgrade1() {
-    let count = parseInt(document.getElementById("money-total").textContent);
-    if (count >= 10) {
+    let count = parseFloat(document.getElementById("money-total").textContent);
+    if (count >= 10 && !document.getElementById("pen-upgrade-1").classList.contains("bought")) {
         count = count - 10;
         document.getElementById("money-total").textContent = count;
-        document.getElementById("pen-upgrade-1").style.display = "none";
-        document.getElementById("pen-upgrade-2").style.display = "block";
-        money_click_value = 2;
+        document.getElementById("pen-upgrade-1").classList.remove("not_bought");
+        document.getElementById("pen-upgrade-1").classList.add("bought");
+        money_click_value = 2.0;
     }
 }
 
 function penUpgrade2() {
-    money_click_value = 4;
+    let count = parseFloat(document.getElementById("money-total").textContent);
+    if (count >= 10 && !document.getElementById("pen-upgrade-2").classList.contains("bought")) {
+        count = count - 10;
+        document.getElementById("money-total").textContent = count;
+        document.getElementById("pen-upgrade-2").classList.remove("not_bought");
+        document.getElementById("pen-upgrade-2").classList.add("bought");
+        money_click_value = 4.0;
+    }
+}
+
+function buyAssist(assistId) {
+    const assist = assists[assistId];
+    let money_total = parseFloat(document.getElementById("money-total").textContent);
+    if (money_total >= assist.price && !assist.active) {
+        money_total -= assist.price;
+        assist.active = true;
+        updateMoneyTotal(money_total);
+
+        assist_rate += assist.rate;
+        document.getElementById(assist.id).classList.remove("not_bought");
+        document.getElementById(assist.id).classList.add("bought");
+    }
+
+    if (assist_started === false) {
+        assist_started = true;
+        addAssist();
+    }
+}
+
+function updateMoneyTotal(money_total) {
+    document.getElementById("money-total").textContent = money_total.toFixed(1);
+}
+
+function addAssist() {
+    setInterval(() => {
+            money_total = parseFloat(document.getElementById("money-total").textContent);
+            money_total += assist_rate;
+            updateMoneyTotal(money_total);
+        }, 1000); // Increment every second
 }
